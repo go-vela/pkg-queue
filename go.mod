5bf1b808e31bba30b8ebcd992b406f88c1802cd9
Neal Coleman <Neal.Coleman@target.com>
2020-03-19 06:17:51 -0500

    chore: add queue interface functions from server

--- go.mod
+++ go.mod
@@ -5,5 +5,7 @@ go 1.13
 require (
 	github.com/go-redis/redis v6.15.7+incompatible
 	github.com/go-vela/types v0.3.0
+	github.com/onsi/ginkgo v1.12.0 // indirect
+	github.com/onsi/gomega v1.9.0 // indirect
 	github.com/sirupsen/logrus v1.4.2
 )
---
+++ queue/redis/push.go
@@ -0,0 +1,10 @@
+// Copyright (c) 2020 Target Brands, Inc. All rights reserved.
+//
+// Use of this source code is governed by the LICENSE file in this repository.
+
+package redis
+
+// Push inserts an item to the specified channel in the queue.
+func (c *client) Push(channel string, item []byte) error {
+	return c.Queue.RPush(channel, item).Err()
+}
