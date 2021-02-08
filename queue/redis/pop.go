07a11f7fab701fd3e638427b425716a0ff96091a
JordanSussman <jordan_sussman@cargill.com>
2021-02-08 15:21:31 -0600

    feat(shutdown): utilize connect timeout to allow non blocking func

--- queue/redis/pop.go
+++ queue/redis/pop.go
@@ -6,7 +6,9 @@ package redis
 
 import (
 	"encoding/json"
+	"time"
 
+	"github.com/go-redis/redis"
 	"github.com/go-vela/types"
 	"github.com/sirupsen/logrus"
 )
@@ -18,14 +20,19 @@ func (c *client) Pop() (*types.Item, error) {
 	// build a redis queue command to pop an item from queue
 	//
 	// https://pkg.go.dev/github.com/go-redis/redis?tab=doc#Client.BLPop
-	popCmd := c.Queue.BLPop(0, c.Channels...)
+	popCmd := c.Queue.BLPop(60*time.Second, c.Channels...)
 
 	// blocking call to pop item from queue
 	//
 	// https://pkg.go.dev/github.com/go-redis/redis?tab=doc#StringSliceCmd.Result
 	result, err := popCmd.Result()
 	if err != nil {
-		return nil, err
+		switch err {
+		case redis.Nil: // BLPOP timeout
+			return nil, nil
+		default:
+			return nil, err
+		}
 	}
 
 	item := new(types.Item)
